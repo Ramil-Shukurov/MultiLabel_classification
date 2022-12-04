639ef5eb4d7e08aafc261519f4a9238cdb47d732
Ramil_Shukurov <116194891+Ramil-Shukurov@users.noreply.github.com>
2022-12-03 23:20:55 -0800

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -1,8 +1,6 @@
 custom_transform = transforms.Compose([
                                        transforms.Resize((200,200)),
                                        transforms.ToTensor(),
-                                       #transforms.Normalize((0.485,0.456,0.406),(0.229,0.224,0.225))
-                                       #transforms.Grayscale(1) # not efficient
                                        ])
 
 path = os.path.join(os.getcwd(), "#4 Garbage_classification")
